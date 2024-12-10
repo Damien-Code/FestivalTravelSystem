8a444ce66375f766adc88bdfc88be31c2074b156
Brighton <s1211945@student.windesheim.nl>
2024-12-10 13:14:24 +0100

    added homepage to nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('homepage')" :active="request()->routeIs('homepage')">
+                        {{ __('Homepage') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('festivals.index')" :active="request()->routeIs('festival.index')">
                         {{ __('Festivals') }}
                     </x-nav-link>
