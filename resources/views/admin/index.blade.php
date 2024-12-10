c672c44d625d863740c19cbc6f0d1a62e29ab230
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-10 13:24:23 +0100

    admin pages, started with routing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<x-app-layout>
+    <div>
+        <div>
+            <p>Gebruikers</p>
+            <a href="{{route('admin.show_users')}}">
+                <button>Edit</button>
+            </a>
+        </div>
+        <div>
+            <p>Festivals</p>
+            <a href="{{route('admin.show_festivals')}}">
+                <button>Edit</button>
+            </a>
+        </div>
+        <div>
+            <p>Busreizen</p>
+            <a href="{{route('admin.show_busses')}}">
+                <button>Edit</button>
+            </a>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/vip/index.blade.php
+++ resources/views/vip/index.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout>
     <h1 class="text-6xl text-white">VIP</h1>
+    <div>
+        <h1>VIP membership</h1>
+    </div>
+    <div>
+        <h1>Voordelen VIP membership</h1>
+    </div>
 </x-app-layout>
