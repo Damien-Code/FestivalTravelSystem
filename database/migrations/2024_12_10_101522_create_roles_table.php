c0feeaa8d27aeb8302076c6398e32f93fc811cc1
Brighton <s1211945@student.windesheim.nl>
2024-12-10 12:30:46 +0100

    removed seeded planner role

--- database/migrations/2024_12_10_101522_create_roles_table.php
+++ database/migrations/2024_12_10_101522_create_roles_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
         DB::table('roles')->insert([
             ['role_name' => 'admin', 'created_at' => now(), 'updated_at' => now()],
             ['role_name' => 'user', 'created_at' => now(), 'updated_at' => now()],
-            ['role_name' => 'planner', 'created_at' => now(), 'updated_at' => now()],
             ['role_name' => 'driver', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
