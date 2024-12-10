99da2eb03837baa068528dcf0faa8d04c46d2d79
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-10 14:00:44 +0100

    finished show_users for admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,21 +1,21 @@
 <x-app-layout>
-    <div>
-        <div>
+    <div class="flex justify-center items-center text-white h-screen gap-10">
+        <div class="h-56 w-56 bg-blue-900">
             <p>Gebruikers</p>
             <a href="{{route('admin.show_users')}}">
-                <button>Edit</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
             </a>
         </div>
-        <div>
+        <div class="h-56 w-56 bg-blue-900">
             <p>Festivals</p>
             <a href="{{route('admin.show_festivals')}}">
-                <button>Edit</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
             </a>
         </div>
-        <div>
+        <div class="h-56 w-56 bg-blue-900">
             <p>Busreizen</p>
             <a href="{{route('admin.show_busses')}}">
-                <button>Edit</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
             </a>
         </div>
     </div>
--- resources/views/admin/show_users.blade.php
+++ resources/views/admin/show_users.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+<x-app-layout>
+    <div class="text-white">
+        <p>Gebruikers</p>
+        <div>
+            <p>Username</p>
+            <p>Email</p>
+            <form method="post">
+                <input type="checkbox">
+            </form>
+        </div>
+    </div>
+    <a href="{{route('admin.index')}}"><button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back</button></a>
+</x-app-layout>
+
+
