b1cc61add23a76eb6c5e293eaeb13f4754016dad
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-10 14:17:54 +0100

    New layout for festival.index

--- resources/views/festivals/index.blade.php
+++ resources/views/festivals/index.blade.php
@@ -4,5 +4,11 @@
             {{ __('Festivals') }}
         </h2>
     </x-slot>
-
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 pt-6">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
