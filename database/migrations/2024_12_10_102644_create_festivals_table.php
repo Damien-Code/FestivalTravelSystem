ace3aeda97d58b432711a356e4a98b47f6ad6d1a
Brighton <s1211945@student.windesheim.nl>
2024-12-11 12:20:01 +0100

    drop correct table name

--- database/migrations/2024_12_10_102644_create_festivals_table.php
+++ database/migrations/2024_12_10_102644_create_festivals_table.php
@@ -33,7 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('info_festivals');
+        Schema::dropIfExists('festival_info');
         Schema::dropIfExists('festivals');
     }
 };
