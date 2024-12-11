efd7ea5d249333d7377a34614fa0d9ed73fa7f56
Brighton <s1211945@student.windesheim.nl>
2024-12-11 11:38:51 +0100

    changed table name to singular

--- database/migrations/2024_12_10_102644_create_festivals_table.php
+++ database/migrations/2024_12_10_102644_create_festivals_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('info_festivals', function (Blueprint $table) {
+        Schema::create('festival_info', function (Blueprint $table) {
             $table->id();
             $table->string('title', 45);
             $table->text('description');
