a232205e01ccc049bd54c1a3f33c97cb75c3db4c
Mischa Sasse <mmischaaaa4@gmail.com>
2024-12-11 15:22:00 +0100

    added todos in homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -11,8 +11,10 @@
                 <form method="get" action="#" class="flex flex-col w-56 text-white">
                     @csrf
                     <label>Festival</label>
+                    {{-- TODO: add dropdown  --}}
                     <input>
-                    <label>Startlocatie</label>
+                    <label>Datum</label> 
+                    {{-- TODO: add calendar popup --}}
                     <input>
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Bekijk busreizen</button>
                 </form>
