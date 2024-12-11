a981511a9add3e49a6ec2f40c1072b4883bab9b2
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-11 15:20:42 +0100

    reformatted code

--- resources/views/festivals/order.blade.php
+++ resources/views/festivals/order.blade.php
@@ -13,7 +13,10 @@
                     <input>
                     <label>Prijs</label>
                     <input>
-                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Bestel Ticket</button>
+                    <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Bestel Ticket
+                    </button>
                 </form>
             </div>
         </div>
