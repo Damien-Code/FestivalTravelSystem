b8814a8753a0e558ab2b73f6bd22098ffc1b42cf
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-13 11:43:04 +0100

    changed vip page layout

--- resources/views/vip/index.blade.php
+++ resources/views/vip/index.blade.php
@@ -4,16 +4,21 @@
             {{ __('VIP') }}
         </h2>
     </x-slot>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 pt-6 text-white">
+    <div class="max-w-xl sm:px-6 lg:px-8 space-y-6 pt-6 text-white">
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
             <div class="max-w-xl">
                 <p>VIP Membership</p>
             </div>
         </div>
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <div class="max-w-xl">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg h-56">
+                <div class="max-w-xl">
                 <p>Voordelen VIP Membership</p>
             </div>
         </div>
+    </div>
+    <div>
+        <figure>
+        
+        </figure>
     </div>
 </x-app-layout>
