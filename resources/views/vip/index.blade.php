a1ccc5a23ded0c6a6d398423612e7dc29baf8c71
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-13 11:50:24 +0100

    finished main styling and added test image

--- resources/views/vip/index.blade.php
+++ resources/views/vip/index.blade.php
@@ -4,21 +4,23 @@
             {{ __('VIP') }}
         </h2>
     </x-slot>
-    <div class="max-w-xl sm:px-6 lg:px-8 space-y-6 pt-6 text-white">
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                <p>VIP Membership</p>
-            </div>
-        </div>
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg h-56">
+    <div class="flex justify-between p-12">
+        <div class="max-w-xl sm:px-6 lg:px-8 space-y-6 pt-6 text-white w-2/5">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                <p>Voordelen VIP Membership</p>
+                    <p>VIP Membership</p>
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg h-56">
+                <div class="max-w-xl">
+                    <p>Voordelen VIP Membership</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div>
-        <figure>
-        
-        </figure>
+        <div>
+            <figure>
+                <img src="{{url('/assets/img.png')}}" alt="test">
+            </figure>
+        </div>
     </div>
 </x-app-layout>
