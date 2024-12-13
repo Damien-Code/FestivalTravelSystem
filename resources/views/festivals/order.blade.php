f938cdc94815df8f7da3a97a3191d3850be255a0
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-13 12:32:56 +0100

    fixed form styling

--- resources/views/festivals/order.blade.php
+++ resources/views/festivals/order.blade.php
@@ -1,17 +1,17 @@
 <x-app-layout>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 pt-6 text-white">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 pt-6">
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
             <div class="max-w-xl">
-                <form class="flex flex-col w-56 text-white" action="{{route('festivals.index')}}">
-                    <label>Naam</label>
+                <form class="flex flex-col w-56" action="{{route('festivals.index')}}">
+                    <label class="text-white">Naam</label>
                     <input class="rounded-lg">
-                    <label>Email</label>
+                    <label class="text-white">Email</label>
                     <input class="rounded-lg">
-                    <label>Festival</label>
+                    <label class="text-white">Festival</label>
                     <input class="rounded-lg">
-                    <label>VIP punten</label>
+                    <label class="text-white">VIP punten</label>
                     <input class="rounded-lg">
-                    <label>Prijs</label>
+                    <label class="text-white">Prijs</label>
                     <input class="rounded-lg mb-8">
                     <x-primary-button>Bestel Ticket</x-primary-button>
                 </form>
