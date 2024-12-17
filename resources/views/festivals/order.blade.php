a268d173067ce4059beb85d8535d228a00ab0ee4
Ismael <noahh.lovell@gmail.com>
2024-12-17 14:22:20 +0100

    Ticket order form: - Name, email & festival on readonly - Removed duplicate price input - Set max on tickets per order

--- resources/views/festivals/order.blade.php
+++ resources/views/festivals/order.blade.php
@@ -4,17 +4,18 @@
             <div class="max-w-xl">
                 <form class="flex flex-col w-56" action="{{route('festivals.index')}}">
                     <label class="text-white">Naam</label>
-                    <input class="rounded-lg">
+                    <input class="rounded-lg bg-neutral-300 text-gray-400" value="{{auth()->user()->name ?? "no name"}}" readonly>
                     <label class="text-white">Email</label>
-                    <input class="rounded-lg">
+                    <input class="rounded-lg bg-neutral-300 text-gray-400" value="{{auth()->user()->email ?? "no email"}}" readonly>
+                    <!--NEEDS TO BE PULLED FROM DB MODEL-->
                     <label class="text-white">Festival</label>
-                    <input class="rounded-lg">
+                    <input class="rounded-lg bg-neutral-300 text-gray-400" value="{{"TODO"}}" readonly>
+                    <!--NEEDS TO BE A CHECKBOX-->
                     <label class="text-white">VIP punten</label>
-                    <input class="rounded-lg">
-                    <label class="text-white">Prijs</label>
-                    <input class="rounded-lg">
+                    <input class="rounded-lg bg-neutral-300 text-gray-400" value="{{"TODO"}}">
                     <label class="text-white">Aantal:</label>
-                    <input type="number" class="rounded-lg" value="1" min="1">
+                    <input type="number" class="rounded-lg" value="1" MIN="1" MAX="35">
+                    <!--NEEDS TO BE CALCULATED WITH TICKET * AMOUNT * VIP DISCOUNT(optional)-->
                     <label class="text-white">Prijs:</label>
                     <input class="rounded-lg mb-6" type="text" disabled>
                     <x-primary-button>Bestel Ticket</x-primary-button>
