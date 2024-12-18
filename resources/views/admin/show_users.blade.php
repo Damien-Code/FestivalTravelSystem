24f817d45f09b59cb9ce94caf45c2998c2e8a6ed
Mischa Sasse <mmischaaaa4@gmail.com>
2024-12-18 09:10:56 +0100

    added base table to show users

--- resources/views/admin/show_users.blade.php
+++ resources/views/admin/show_users.blade.php
@@ -2,19 +2,47 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 pt-6 text-white">
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
             <div class="max-w-xl">
-                <p>Gebruikers</p>
-                <div>
-                    <p>Username</p>
-                    <p>Email</p>
-                    <form method="post">
-                        <input type="checkbox">
-                    </form>
+                <h2>Gebruikers</h2>
+
+
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Username
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Email
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Is Admin
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Username
+                                </th>
+                                <td class="px-6 py-4">
+                                    Email
+                                </td>
+                                <td class="px-6 py-4">
+                                    <form method="post">
+                                        <input type="checkbox">
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
             <div class="max-w-xl">
-                <a href="{{route('admin.index')}}">
+                <a href="{{ route('admin.index') }}">
                     <x-primary-button>Back</x-primary-button>
                 </a>
             </div>
@@ -22,5 +50,3 @@
     </div>
 
 </x-app-layout>
-
-
