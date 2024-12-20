b9b2c78ccae93be8967177ecaf199d6a0dd3cf10
Damien van den IJssel <dovandenijssel@live.nl>
2024-12-20 17:00:15 +0100

    Added label and different layout

--- resources/views/admin/festivals/create_festivals.blade.php
+++ resources/views/admin/festivals/create_festivals.blade.php
@@ -6,30 +6,32 @@
             </div>
         </div>
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-between min-w-full">
-            <div class="max-w-xl h-56">
+            <div class="max-w-xl h-72">
                 @if($errors->any())
                     {!! implode('', $errors->all('<div>:message</div>')) !!}
                 @endif
                 <form action="{{route('festivals.store')}}" method="post" enctype="multipart/form-data"
                       class="text-black flex flex-col justify-evenly min-h-full">
                     @csrf
+                    <label class="text-white text-2xl">Create a new festival:</label>
                     <input type="text" name="title" class="rounded-lg" placeholder="Title">
                     <input type="text" name="description" class="rounded-lg" placeholder="Description">
                     <input type="file" name="image" class="rounded-lg bg-gray-50">
                     <x-primary-button>Save</x-primary-button>
                 </form>
             </div>
-            <div class="max-w-xl h-56">
+            <div class="max-w-xl h-72">
                 <form action="{{route('festivals.new')}}" method="post"
                       class="text-black flex flex-col justify-evenly min-h-full">
                     @csrf
+                    <label class="text-white text-2xl">Pair a festival:</label>
                     <select name="festival" class="rounded-lg">
                         @foreach($festivalsInfo as $info)
                             <option value="{{$info->id}}" class="text-black">{{$info->title}}</option>
                         @endforeach
                     </select>
                     {{--                    Location will be used after location is finished--}}
-                    {{--                    <input type="text" name="title" class="rounded-lg" placeholder="Location">--}}
+                    <input type="text" class="rounded-lg" placeholder="Location">
                     <input type="date" name="date" class="rounded-lg" placeholder="Date">
                     <x-primary-button>Save</x-primary-button>
                 </form>
