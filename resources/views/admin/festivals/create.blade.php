38aafd5d8d21914fc6ba15a3b8d75849fa2c0887
Damien van den IJssel <dovandenijssel@live.nl>
2025-01-08 09:00:44 +0100

    flash message made as layout

--- resources/views/admin/festivals/create.blade.php
+++ resources/views/admin/festivals/create.blade.php
@@ -5,17 +5,9 @@
                 <p>Festivals</p>
             </div>
         </div>
-        @if(session()->has('success'))
-            <div id="alert-1" class="flex items-center p-4 mb-4 rounded-lg bg-green-300 text-black" role="alert">
-                {{session('success')}}
-                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-1" aria-label="Close">
-                    <span class="sr-only">Close</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                </button>
-            </div>
-        @endif
+        {{--        Flash message--}}
+        @include('layouts.success')
+
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-between min-w-full">
             <div class="max-w-xl h-72">
                 @if($errors->any())
