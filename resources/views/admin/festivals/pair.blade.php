bc17c5c95cb5e00e06a95dafd2eca0a887f8a529
Damien van den IJssel <dovandenijssel@live.nl>
2025-01-10 11:09:52 +0100

    new form for pairing festival

--- resources/views/admin/festivals/pair.blade.php
+++ resources/views/admin/festivals/pair.blade.php
@@ -10,25 +10,33 @@
         </div>
         {{-- flash success message --}}
         @include('layouts.success')
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-between min-w-full">
-            <div class="max-w-xl h-72">
-                {{-- Form to plan a festival and link it to a date --}}
-                <form action="{{route('admin.festivals.planFestival')}}" method="post"
-                      class="text-black flex flex-col justify-evenly min-h-full">
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Pair an existing festival to a date and location</h2>
+                <form action="{{route('admin.festivals.planFestival')}}" method="post">
                     @csrf
                     @method('POST')
-                    <label class="text-white text-2xl">Pair a festival:</label>
-                    <select name="festival" class="rounded-lg">
-                        @foreach($festivalsInfo as $info)
-                            <option value="{{$info->id}}" class="text-black">{{$info->title}}</option>
-                        @endforeach
-                    </select>
-                    {{-- Location will be used after location is finished --}}
-                    <input type="text" class="rounded-lg" placeholder="Location">
-                    <input type="date" name="date" min="{{date('Y-m-d')}}" value="{{date('Y-m-d')}}" class="rounded-lg">
-                    <x-primary-button>Save</x-primary-button>
+                    <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                        <div class="sm:col-span-2">
+                            <label for="festival" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Festival</label>
+                            <select name="festival" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                @foreach($festivalsInfo as $info)
+                                    <option value="{{$info->id}}">{{$info->title}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="What is the location?">
+                        </div>
+                        <div>
+                            <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                            <input type="date" name="date" min="{{date('Y-m-d')}}" value="{{date('Y-m-d')}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        </div>
+                    </div>
+                    <x-primary-button class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center">Add</x-primary-button>
                 </form>
             </div>
-        </div>
+        </section>
     </div>
 </x-app-layout>
