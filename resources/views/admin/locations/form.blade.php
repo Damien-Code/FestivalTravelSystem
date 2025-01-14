2678c8d7a1ac0581a5a1268d7b7dc5c78107b304
Brighton <s1211945@student.windesheim.nl>
2025-01-14 12:19:55 +0100

    changed incorrect label

--- resources/views/admin/locations/form.blade.php
+++ resources/views/admin/locations/form.blade.php
@@ -291,7 +291,7 @@
                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         </div>
                         <div class="sm:col-span-2">
-                            <label for="street" class="default text">City</label>
+                            <label for="street" class="default text">Street</label>
                             <input type="text" name="street" placeholder="Street name" value="{{old('street', $location->street)}}"
                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         </div>
