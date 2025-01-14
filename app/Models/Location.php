86522024a1e416186226e1cebe31d9dc37359e9a
Brighton <s1211945@student.windesheim.nl>
2025-01-14 12:32:55 +0100

    softdeletes in model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Location extends Model
 {
     /** @use HasFactory<\Database\Factories\LocationFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'country',
