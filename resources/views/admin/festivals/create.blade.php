e950593e7fe872d380fff1f6c3991806296a98af
Damien van den IJssel <dovandenijssel@live.nl>
2025-01-17 10:13:00 +0100

    Fixed the post data too large

--- resources/views/admin/festivals/create.blade.php
+++ resources/views/admin/festivals/create.blade.php
@@ -27,9 +27,9 @@
                         </div>
                         <div class="sm:col-span-2">
                             <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
-                            <input type="file" name="image"
+                            <input type="file" name="image" id="image"
                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-{{--                                   onchange="getFileSize(this)"--}}
+                                    {{--                                   onchange="getFileSize(this)"--}}
                             >
                             <p>Files supported: JPEG, PNG and JPG. Max-size: 5mb</p>
                         </div>
@@ -57,4 +57,22 @@
     {{--            }--}}
     {{--        }--}}
     {{--    </script>--}}
+    <script>
+        const file = document.getElementById('image')
+        file.addEventListener('change', (event) => {
+            const target = event.target
+            if (target.files && target.files[0]) {
+                const maxAllowedSize = 5 * 1024 * 1024;
+                if (target.files[0].size > maxAllowedSize) {
+                    target.value = ''
+                    file.classList.remove('dark:border-gray-600')
+                    file.classList.remove('border-green-500')
+                    file.classList.add('border-rose-500')
+                } else {
+                    file.classList.remove('dark:border-gray-600')
+                    file.classList.add('border-green-500')
+                }
+            }
+        })
+    </script>
 </x-app-layout>
