04e08ff2e272c98b24c7d7d103597e4394d21fa3
Ismael <noahh.lovell@gmail.com>
2025-01-17 15:22:27 +0100

    Fix how location is displayed on order with back-end change Updated styling of dashboard: + colomns: order-id, order date, festival, start location, price, discount

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,42 +4,71 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-<div class="flex justify-between">
-    <div class="py-12">
-        <div class="max-w-7xl sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-8 w-96 h-16">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p>Good morning, {{auth()->user()->name }} </p>
-                </div>
-            </div>
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-96 h-24">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p>Saved up points:</p>
-                    <p class="text-3xl">{{ auth()->user()->tokens }}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="max-w-7xl sm:px-6 lg:px-8 w-3/5 pt-12">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-8">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class="bg-white dark:bg-gray-900 overflow-hidden shadow-sm sm:rounded-lg mb-8 w-4/5 mx-auto p-6">
-                    <p class="text-2xl ">Travel history</p>
-                </div>
-{{--                TODO: load all orders in--}}
-                @foreach($orders as $order)
-                    <div class="flex justify-between bg-gray-900 rounded-lg p-6 mb-6">
-                        <p>Festival: {{ $order->route->festival->festivalInfo->title }}</p>
-                        <p>Start location: {{ $order->route->location->address() }}</p>
-                        <p>Order date: {{ $order->created_at }}</p>
-                        <p>Tickets bought: {{ $order->amount_of_tickets }}</p>
-                        <p>Price: {{ $order->final_price }} </p>
-                        <p>Used discount: @if($order->tokens_used > 0) Yes @else No @endif</p>
+    <section class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+        <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+            <div class="mx-auto max-w-5xl">
+                <div class="gap-4 sm:flex sm:items-center sm:justify-between">
+                    <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">My orders</h2>
+                    {{--Total points--}}
+                    <div class="mt-6 gap-4 space-y-4 sm:mt-0 sm:flex sm:items-center sm:justify-end sm:space-y-0">
+                        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-2 text-gray-900 dark:text-gray-100">
+                                <p>Your saved up points:</p>
+                                <p class="text-3xl">{{ auth()->user()->tokens }}</p>
+                            </div>
+                        </div>
                     </div>
-                @endforeach
+                </div>
+                {{--List of all the orders--}}
+                <div class="mt-6 flow-root sm:mt-8">
+                    <div class="divide-y divide-gray-200 dark:divide-gray-700">
+                        @foreach($orders as $order)
+                            <div class="flex flex-wrap items-center gap-y-4 py-6">
+                            <dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+                                <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Order ID:</dt>
+                                <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">#{{ $order->id }}</dd>
+                            </dl>
+                            <dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+                                <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Order date:</dt>
+                                <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{{ $order->created_at->format('Y-m-d') }}</dd>
+                            </dl>
+                            <dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+                                <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Festival:</dt>
+                                <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{{ $order->route->festival->festivalInfo->title }}</dd>
+                            </dl>
+                            <dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+                                <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Start location:</dt>
+                                <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{{ $order->route->location->address() }}</dd>
+                            </dl>
+                            <dl class="w-1/2 pl-0 sm:w-1/4 lg:w-auto lg:flex-1 lg:pl-4">
+                                <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Price:</dt>
+                                <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">€{{ $order->final_price }}</dd>
+                            </dl>
+                            <dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+                                <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Discount:</dt>
+                                @if($order->tokens_used > 0)
+                                    {{--SVG Green rectangle with a checkmark saying yes--}}
+                                    <dd class="me-2 mt-1.5 inline-flex items-center rounded bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-green-900 dark:text-green-300">
+                                        <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                        </svg>
+                                        Yes
+                                    </dd>
+                                @else
+                                    {{--SVG Red rectangle with an X saying no--}}
+                                    <dd class="me-2 mt-1.5 inline-flex items-center rounded bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800 dark:bg-red-900 dark:text-red-300">
+                                        <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
+                                        </svg>
+                                        No
+                                    </dd>
+                                @endif
+                            </dl>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
+    </section>
 </x-app-layout>
