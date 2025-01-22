85dc85a880c9e9c296be0a7b02b1ce74f98d1c1e
Mischa Sasse <mmischaaaa4@gmail.com>
2025-01-22 15:16:13 +0100

    added unit tests

--- tests/Unit/AdminUserUnitTest.php
+++ tests/Unit/AdminUserUnitTest.php
@@ -11,16 +11,41 @@ use Tests\TestCase;
 class AdminUserUnitTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * @author Mischa Sasse
+     * 
+     * This test creates a user,
+     * then it updates its role
+     * then it checks if the role has been changed in the database
      */
     public function test_change_user_role(): void
     {
         $user = User::factory()->create(['role_id' => 1]);
-        $response = $this->actingAs($user)
+        $this->actingAs($user)
             ->patch(route('admin.users.update',$user->id), [
                 'role_id' => 2
             ]);
 
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+                'id' => $user->id,
+                'role_id' => 2
+            ]);
+        // $response->assertStatus(200);
+    }
+
+    /** 
+     * @author Mischa Sasse
+     * 
+     * This test creates 2 users,
+     * then user1 tries to delete user2
+     */
+    public function test_delete_user(): void
+    {
+        $user1 = User::factory()->create(['role_id' => 1]);
+        $user2 = User::factory()->create(['role_id' => 1]);
+            $this->actingAs($user1)
+                ->delete(route('admin.users.destroy', $user2->id));
+            $this->assertSoftDeleted('users', [
+                'id' => $user2->id,
+            ]);
     }
 }
