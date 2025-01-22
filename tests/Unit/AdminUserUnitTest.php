7a2ce2e347e38586d40aaa1ab266f10db559a5e2
Mischa Sasse <mmischaaaa4@gmail.com>
2025-01-22 14:24:08 +0100

    unit test added

--- tests/Unit/AdminUserUnitTest.php
+++ tests/Unit/AdminUserUnitTest.php
@@ -2,15 +2,25 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Festival;
+use App\Models\Location;
+use App\Models\User;
+use Tests\TestCase;
+
 
 class AdminUserUnitTest extends TestCase
 {
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    public function test_change_user_role(): void
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create(['role_id' => 1]);
+        $response = $this->actingAs($user)
+            ->patch(route('admin.users.update',$user->id), [
+                'role_id' => 2
+            ]);
+
+        $response->assertStatus(200);
     }
 }
