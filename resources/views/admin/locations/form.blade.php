e2bb5371b633c69ff85d00c5455047ef011e9f14
Brighton <s1211945@student.windesheim.nl>
2025-01-24 12:20:19 +0100

    changed html in location blade to relfect if updating or creating

--- resources/views/admin/locations/form.blade.php
+++ resources/views/admin/locations/form.blade.php
@@ -291,8 +291,13 @@
                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         </div>
                     </div>
-                    <x-primary-button class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6">Add location
-                    </x-primary-button>
+                    @if($location->exists)
+                        <x-primary-button class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6">Update location
+                        </x-primary-button>
+                    @else
+                        <x-primary-button class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6">Add location
+                        </x-primary-button>
+                    @endif
                 </form>
             </div>
         </section>
